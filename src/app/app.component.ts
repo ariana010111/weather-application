94952f24262f4bb4bc618fb0139e58175234ad27
unknown <s.soltani@whiteboard-studio.nl>
2023-08-15 04:41:47 +0330

    issue in first load fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   title = 'weather-ng-app';
 
   constructor(private primengConfig: PrimeNGConfig,
+
              ) {}
 
   ngOnInit(): void {
--- src/app/core/components/top-bar/top-bar.component.ts
+++ src/app/core/components/top-bar/top-bar.component.ts
@@ -14,26 +14,25 @@ export class TopBarComponent implements OnInit {
 items: MegaMenuItem[] | undefined;
 
   ngOnInit() {
-    this.translateService.load('en').subscribe();
-    this.translateService.events$.pipe(filter(event => event.type === 'translationLoadSuccess')).subscribe(() =>{
+
       this.items = [
         {
-          label: this.translateService.translate('bundleID_literal_home'),
+          label: 'Home',
           icon: 'pi pi-fw pi-home',
           url: ''
         },
         {
-          label:  this.translateService.translate('bundleID_literal_weather'),
+          label:  'Weather',
           icon: 'pi pi-fw pi-sun',
           url: '/weather'
         },
         {
-          label: this.translateService.translate('bundleID_literal_setting'),
+          label: 'Setting',
           icon: 'pi pi-fw pi-cog',
           url: '/setting'
         }
       ];
-    })
+
 
   }
 }
